0961daa2c0f94bc0726032f931d64d5e7e3aa738
rabedrafitra <166728136+rabedrafitra@users.noreply.github.com>
2024-04-19 17:22:34 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com","localhost","esther-edu.vercel.app"]
+        domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com","localhost","oqpwxy2uhkydedit.public.blob.vercel-storage.com"]
     }
 }
 
