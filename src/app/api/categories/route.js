1c1503fcae886213612678be736ff6aa28c48b02
Rabedrafitra <166728136+rabedrafitra@users.noreply.github.com>
2024-05-15 16:25:39 +0300

    Update route.js

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
   try {
     const categories = await prisma.category.findMany();
-
+    console.log(categories);
     return new NextResponse(JSON.stringify(categories, { status: 200 }));
   } catch (err) {
     console.log(err);
